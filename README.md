9b2361079ec684aaa9b2342c4bb6540696b50492
easybodge <office@easybodge.com>
2021-12-31 01:53:35 +0000

    Completed Simple Interest Exercise

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # coding-project-template
+I haven't been able to highlight the variables with yellow background. If you can advise on how to do this, that would be great. Rest of it is functional. 
--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+
+
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
+{ 
+    var PRINCIPAL = document.getElementById("principal").value;
+    var RATE = document.getElementById("rate").value;
+    var YEARS = document.getElementById("years").value;
+    var AMOUNT = (RATE/100*PRINCIPAL)*YEARS;
+    var PARA = document.createElement("P");
+    PARA.innerHTML = "If you deposit "+PRINCIPAL + "<br/>" + "at an interest rate of " + RATE + "%. <br/>"+ "You will receive an amount of "+ AMOUNT + ",<br/>" + "in the year 202" + YEARS;
+
+    console.log(PRINCIPAL,RATE,YEARS,AMOUNT);
+    document.getElementById("RESULT").innerHTML=PARA.innerHTML;
 }
-        
