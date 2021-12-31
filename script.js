5c37797725188ae92ba252a292bf9c59cf428061
easybodge <office@easybodge.com>
2021-12-31 12:28:24 +0000

    changed 'YEARS' control to drop down menu

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 
 
-function compute()
+function Compute()
 { 
     var PRINCIPAL = document.getElementById("principal").value;
     var RATE = document.getElementById("rate").value;
-    var YEARS = document.getElementById("years").value;
+    var YEARS = parseInt(document.getElementById("years").value);
     var AMOUNT = (RATE/100*PRINCIPAL)*YEARS;
     var PARA = document.createElement("P");
-    PARA.innerHTML = "If you deposit "+PRINCIPAL + "<br/>" + "at an interest rate of " + RATE + "%. <br/>"+ "You will receive an amount of "+ AMOUNT + ",<br/>" + "in the year 202" + YEARS;
+    var FUTUREYEAR= new Date().getFullYear() + YEARS;
+    PARA.innerHTML = "If you deposit <mark>"+ PRINCIPAL+"</mark>+ <br/>" + "at an interest rate of <mark>" + RATE + "%.</mark> <br/>"+ "You will receive an amount of <mark>"+ AMOUNT + "</mark>,<br/>" + "in the year <mark>" + FUTUREYEAR + "</mark>";
 
     console.log(PRINCIPAL,RATE,YEARS,AMOUNT);
     document.getElementById("RESULT").innerHTML=PARA.innerHTML;
 }
+
+function Validate_Amount()
+{
+    var PRINCIPAL=document.getElementById("principal").value;
+    if (PRINCIPAL<=0 || PRINCIPAL==NaN) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    } 
+    
+}
+
